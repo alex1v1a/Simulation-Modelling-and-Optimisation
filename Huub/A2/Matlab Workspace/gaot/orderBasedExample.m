ebf4a3ff9ad6723f818dd9813d1443555b0c3e48
Alex <alex@1v1a.com>
2017-09-15 15:12:17 +1200

    updates

--- Huub/A2/Matlab Workspace/gaot/orderBasedExample.m
+++ Huub/A2/Matlab Workspace/gaot/orderBasedExample.m
@@ -41,7 +41,7 @@ mOpts = [2;2;2;2;2];
 
 % Termination Operators
 termFns = 'maxGenTerm';
-termOps = [10000]; % 200 Generations
+termOps = [1000]; % 200 Generations
 
 % Selection Function
 selectFn = 'normGeomSelect';
@@ -60,19 +60,21 @@ bounds = [sz];
 gaOpts=[1e-6 1 1];
 
 % Generate an intialize population of size 20 & use scoring
-startPop = initializeoga(150,bounds,'tracker',[1e-6 1]);
+startPop = initializeoga(1000,bounds,'tracker',[1e-6 1]);
 
 % Lets run the GA
 %Hit a return to continue
-pause
+% pause
 [x endPop bestPop trace]=ga(bounds,evalFn,evalOps,startPop,gaOpts,...
     termFns,termOps,selectFn,selectOps,xFns,xOpts,mFns,mOpts);
 
+echo on
 % x is the best solution found
 x
 BestValue = 1000 +x(end)
-%Hit a return to continue
+% Hit a return to continue
 pause
+echo off
 
 % endPop is the ending population
 %endPop
@@ -82,25 +84,25 @@ pause
 % bestPop is the best solution tracked over generations
 bestPop
 %Hit a return to continue
-pause
+% pause
 
 % trace is a trace of the best value and average value of generations
 trace
 
 %Hit a return to continue
-pause
+% pause
 
 % Plot the best over time
 clf
 plot(trace(:,1),trace(:,2));
 %Hit a return to continue
-pause
+% pause
 
 % Add the average to the graph
 hold on
 plot(trace(:,1),trace(:,3));
 %Hit a return to continue
-pause
+% pause
  
 figure(2)
 clf
@@ -108,12 +110,23 @@ A=ones(sz,sz);
 A= xor(triu(A),tril(A));
 [xg yg]=gplot(A,t);
 clf
-plot(xg,yg,'b:','MarkerSize',24);
+% plot(xg,yg,'b:','MarkerSize',24);
 h=gca;
 hold on
 ap=x;
-plot(t(x(1:sz),1),t(x(1:(sz)),2),'r-') 
-plot(t([x(1),x(sz)],1),t([x(1),x(sz)],2),'r-') 
+output = 1;
+for i = 1:28
+    output = [output x(i)];
+    if x(i) == 1
+        break
+    end
+end
+disp(x)
+disp(output)
+% pause
+outSz = size(output,2)
+plot(t(output(1:outSz),1),t(output(1:(outSz)),2),'r-') 
+% plot(t([x(1),x(sz)],1),t([x(1),x(sz)],2),'r-')
 plot(xg,yg,'b.','MarkerSize',24);
 
 j=1;
@@ -129,3 +142,5 @@ for i=1:sz
  end
  legend('Path','Best Found Path')
 echo off
+x
+BestValue = 1000 +x(end)
